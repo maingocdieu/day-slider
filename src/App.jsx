cfe1dc6936163557af5eeb0f6158cccd5162efc7
DESKTOP-GVD5U6A\Admin <maingocdieu032@gmail.com>
2023-10-31 08:47:08 +0700

    x

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,11 @@ function App() {
    */
   const ref2 = useRef();
 
+  /**
+   * @type {React.MutableRefObject<HTMLElement>}
+   */
+  const ref3 = useRef();
+
   const handleMouseDown = (e) => {
     ref.current.style.background = "red";
   };
@@ -23,11 +28,18 @@ function App() {
   const handleMouseMove = (e) => {
     let i = 0;
     i = e.clientX - ref2.current.getBoundingClientRect().left;
-    if (i > 0 && i < 600) {
+    if (i > -32 && i < 600) {
       ref2.current.style.width = `${i}px`;
-      ref2.current.style.zIndex = 8;
-
+      ref2.current.style.zIndex = 0;
       ref.current.style.left = `${i}px`;
+      if (i > 100) {  
+          ref3.current.style.borderColor = "blue";
+          ref3.current.style.zIndex = 5;
+          ref3.current.style.background = "red";
+          ref2.current.style.zIndex = 0;
+      } else if(i<100) {
+        ref3.current.style.borderColor = "black";
+      }
     }
   };
 
@@ -36,12 +48,12 @@ function App() {
       <div className="container">
         <div className="straight" onMouseMove={handleMouseMove}>
           <div className="day-30">
-            <div className="circle"></div>
+            <div className="circle" ></div>
             <span className="day">30 days</span>
           </div>
 
           <div className="day-60">
-            <div className="circle"></div>
+            <div className="circle" ref ={ref3}></div>
             <span className="day">60 days</span>
           </div>
         </div>
